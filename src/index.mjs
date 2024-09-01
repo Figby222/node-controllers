3f1d89d8af534dd0fd51709cc5a33e3673357195
Figby222 <figby222@outlook.com>
2024-08-31 20:08:15 -0700

    Redirect /user path to userRouter

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
+import userRouter from "./routes/userRouter.mjs";
 const app = express();
+
+app.use("/user", userRouter);
 
 function myMiddleware(req, res, next) {
     console.log("Middleware function called");
