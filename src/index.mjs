dae8e1e4cdc4e20d37b0ca4f8251f2ce68d4b20e
Figby222 <figby222@outlook.com>
2024-08-31 19:31:36 -0700

    Add function myMiddleware & use on root

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,15 @@ import express from "express";
 import "dotenv/config";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hi"));
+function myMiddleware(req, res, next) {
+    console.log("Middleware function called");
+
+    req.customProperty = "Hello from myMiddleware";
+
+    next();
+}
+
+app.get("/", myMiddleware, (req, res) => res.send("Hi" + req.customProperty));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
