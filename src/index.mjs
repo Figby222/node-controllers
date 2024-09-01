43c9baa15f2773461606b921e9217dfa0d87cb47
Figby222 <figby222@outlook.com>
2024-08-31 21:08:21 -0700

    Place application level middleware above routes
    Place application level middleware above route to
    /user

--- src/index.mjs
+++ src/index.mjs
@@ -3,17 +3,18 @@ import "dotenv/config";
 import userRouter from "./routes/userRouter.mjs";
 const app = express();
 
-app.use("/user", userRouter);
 
 function myMiddleware(req, res, next) {
     console.log("Middleware function called");
-
+    
     req.customProperty = "Hello from myMiddleware";
-
+    
     next();
 }
 
 app.use(myMiddleware);
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => res.send("Hi" + req.customProperty));
 
